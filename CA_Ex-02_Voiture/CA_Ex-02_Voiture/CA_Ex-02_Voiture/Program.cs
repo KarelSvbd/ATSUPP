bfe5e61624bf5051877ed119a0b8ed9ec2064f3a
Karel V. Svoboda <57522089+KarelSvbd@users.noreply.github.com>
2021-11-11 07:35:23 +0100

    11.11.2021

---
+++ CA_Ex-02_Voiture/CA_Ex-02_Voiture/CA_Ex-02_Voiture/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Ex_02_Voiture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "";
+            string marque = "";
+            string model = "";
+            string couleur = "";
+
+            
+
+            Console.Write(" Marque : ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            // Récup des données utilisateurs
+            marque = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write(" Model : ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            // Récup des données utilisateurs
+            model = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write(" Couleur : ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            // Récup des données utilisateurs
+            couleur = Console.ReadLine();
+
+            // Créer la voiture
+            Voiture Voit = new Voiture(marque, model, couleur);
+
+            // Clear la console
+            Console.Clear();
+
+            // Tant que ! de q reste dans la boucle
+            while (data != "q")
+            {
+                // Affiche la voiture
+                Console.WriteLine(Voit);
+
+                Console.WriteLine();
+                // Change la couleur du texte
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("****************");
+                Console.Write(" + | Accelerer"); Console.WriteLine(" *");
+                Console.Write(" - | Decelerer"); Console.WriteLine(" *");
+                Console.Write(" S | Start"); Console.WriteLine("     *");
+                Console.Write(" P | Stop"); Console.WriteLine("      *");
+                Console.Write(" K | Klaxon"); Console.WriteLine("    *");
+                Console.Write(" C | Couleur"); Console.WriteLine("   *");
+                Console.Write(" Q | Quitter"); Console.WriteLine("   *");
+                Console.Write("****************");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+
+                Console.Write("Action : ");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                data = Console.ReadLine();
+                // Efface la console
+                Console.Clear();
+
+                // Utilisation de la varible data pour choisir les options
+                switch (data)
+                {
+                    // Appel la fonction suivant l'option choisis
+                    case "+":
+                        Console.WriteLine(Voit.Accelerer());
+                        break;
+
+                    case "-":
+                        Console.WriteLine(Voit.Freiner());
+                        break;
+
+                    case "s":
+                        Console.WriteLine(Voit.DemarrerMoteur());
+                        break;
+
+                    case "p":
+                        Console.WriteLine(Voit.ArretMoteur());
+                        break;
+
+                    case "k":
+                        Console.WriteLine(Voit.Klaxoner());
+                        break;
+
+                    case "c":
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Nouvelle Couleur : ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        couleur = Console.ReadLine();
+                        Voit.Couleur = couleur;
+                        Console.WriteLine(Voit.Couleur);
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CA_Ex-02_Voiture/CA_Ex-02_Voiture/CA_Ex-02_Voiture/Voiture.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Ex_02_Voiture
+{
+    class Voiture
+    {
+        private DateTime _datefabrication;
+        private string _marque;
+        private string _model;
+        private string _couleur;
+        private bool _etatmoteur;
+        private int _vitesse;
+        private string _klaxon;
+        private int _numeroSerie;
+
+        /// <summary>
+        /// Constructeur ...
+        /// </summary>
+        /// <param name="marque">marque du véhicule</param>
+        /// <param name="model"></param>
+        /// <param name="couleur"></param>
+        public Voiture(string marque, string model, string couleur)
+            :this(marque, model, couleur, new Random().Next(1, 10000))
+        {      
+        }
+
+        public Voiture(string marque, string model, string couleur, int numeroSerie)
+        {
+            _datefabrication = DateTime.Now;
+            _marque = marque;
+            _model = model;
+            _couleur = couleur;
+            _numeroSerie = numeroSerie;
+        }
+
+        public Voiture(string marque, string model):this(marque,model,"Rose Bonbon")
+        {
+        }
+
+        public int NumeroSerie
+        {
+            get { return _numeroSerie; }
+            private set { _numeroSerie = value; }
+        }
+
+
+        public string Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+
+        public string Klaxoner()
+        {
+            return "BIP BIP BIP BIP BIP BOUGE TA CAISSE";
+        }
+
+        public string DemarrerMoteur()
+        {
+            if (_etatmoteur == false)
+            {
+                _etatmoteur = true;
+                return "Vrooooum";
+            }
+            else
+            {
+                return "Grrrrr";
+            }
+        }
+
+        public int Accelerer()
+        {
+            if (_etatmoteur == true)
+            {
+                return _vitesse += 10;
+            }
+            return _vitesse;
+        }
+        public int Freiner()
+        {
+            if (_vitesse <= 20)
+            {
+                return _vitesse = 0;
+            }
+            else
+            {
+                return _vitesse -= 20;
+            }
+                
+        }
+
+        public string ArretMoteur()
+        {
+            if (_etatmoteur != false)
+            {
+                _etatmoteur = false;
+                return "Moteur éteint";
+            }
+            else
+            {
+                return "Moteur déjà éteint";
+            } 
+        }
+
+        public override string ToString()
+        {
+            return _marque + ", " + _model + ", " + _couleur;
+        }
+    }
+}
